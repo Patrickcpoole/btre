9aa351ac6f1a9b7e413c3aed4a23f0ffee6ce493
Patrick Poole <patrickpoole@Patricks-MacBook-Pro.local>
2021-01-03 18:11:49 -0700

    added local settings info

--- btre/settings.py
+++ btre/settings.py
@@ -148,3 +148,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'patrickcpoole@gmail.com'
 EMAIL_HOST_PASSWORD='Whatisdeadmayneverdie'
 EMAIL_USE_TLS  = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
